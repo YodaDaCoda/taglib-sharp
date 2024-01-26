8cd7528f2b451e3fca1ad8e154ccbf6a8484147a
Qstick <qstick@gmail.com>
2024-01-25 20:22:18 -0600

    Fix formatting, line endings and tabs
    Co-Authored-By: ta264 <ta264@users.noreply.github.com>

--- src/TaglibSharp.Tests/Images/JpegOlympus3Test.cs
+++ src/TaglibSharp.Tests/Images/JpegOlympus3Test.cs
@@ -8,127 +8,127 @@ using File = TagLib.File;
 
 namespace TaglibSharp.Tests.Images
 {
-	[TestFixture]
-	public class JpegOlympus3Test
-	{
-		static readonly string sample_file = TestPath.Samples + "sample_olympus3.jpg";
-		static readonly string tmp_file = TestPath.Samples + "tmpwrite_olympus3.jpg";
+    [TestFixture]
+    public class JpegOlympus3Test
+    {
+        static readonly string sample_file = TestPath.Samples + "sample_olympus3.jpg";
+        static readonly string tmp_file = TestPath.Samples + "tmpwrite_olympus3.jpg";
 
-		readonly TagTypes contained_types = TagTypes.TiffIFD;
+        readonly TagTypes contained_types = TagTypes.TiffIFD;
 
-		File file;
+        File file;
 
-		[OneTimeSetUp]
-		public void Init ()
-		{
-			file = File.Create (sample_file);
-		}
+        [OneTimeSetUp]
+        public void Init ()
+        {
+            file = File.Create (sample_file);
+        }
 
-		[Test]
-		public void JpegRead ()
-		{
-			CheckTags (file);
-		}
+        [Test]
+        public void JpegRead ()
+        {
+            CheckTags (file);
+        }
 
-		[Test]
-		public void ExifRead ()
-		{
-			CheckExif (file);
-		}
+        [Test]
+        public void ExifRead ()
+        {
+            CheckExif (file);
+        }
 
-		[Test]
-		public void MakernoteRead ()
-		{
-			CheckMakerNote (file);
-		}
+        [Test]
+        public void MakernoteRead ()
+        {
+            CheckMakerNote (file);
+        }
 
-		[Test]
-		public void PropertiesRead ()
-		{
-			CheckProperties (file);
-		}
+        [Test]
+        public void PropertiesRead ()
+        {
+            CheckProperties (file);
+        }
 
-		[Test]
-		public void Rewrite ()
-		{
-			var tmp = Utils.CreateTmpFile (sample_file, tmp_file);
-			tmp.Save ();
+        [Test]
+        public void Rewrite ()
+        {
+            var tmp = Utils.CreateTmpFile (sample_file, tmp_file);
+            tmp.Save ();
 
-			tmp = File.Create (tmp_file);
+            tmp = File.Create (tmp_file);
 
-			CheckTags (tmp);
-			CheckExif (tmp);
-			CheckMakerNote (tmp);
-			CheckProperties (tmp);
-		}
+            CheckTags (tmp);
+            CheckExif (tmp);
+            CheckMakerNote (tmp);
+            CheckProperties (tmp);
+        }
 
-		[Test]
-		public void AddExif ()
-		{
-			AddImageMetadataTests.AddExifTest (sample_file, tmp_file, true);
-		}
+        [Test]
+        public void AddExif ()
+        {
+            AddImageMetadataTests.AddExifTest (sample_file, tmp_file, true);
+        }
 
-		[Test]
-		public void AddGPS ()
-		{
-			AddImageMetadataTests.AddGPSTest (sample_file, tmp_file, true);
-		}
+        [Test]
+        public void AddGPS ()
+        {
+            AddImageMetadataTests.AddGPSTest (sample_file, tmp_file, true);
+        }
 
-		[Test]
-		public void AddXMP1 ()
-		{
-			AddImageMetadataTests.AddXMPTest1 (sample_file, tmp_file, false);
-		}
+        [Test]
+        public void AddXMP1 ()
+        {
+            AddImageMetadataTests.AddXMPTest1 (sample_file, tmp_file, false);
+        }
 
-		[Test]
-		public void AddXMP2 ()
-		{
-			AddImageMetadataTests.AddXMPTest2 (sample_file, tmp_file, false);
-		}
+        [Test]
+        public void AddXMP2 ()
+        {
+            AddImageMetadataTests.AddXMPTest2 (sample_file, tmp_file, false);
+        }
 
-		public void CheckTags (File file)
-		{
-			Assert.IsTrue (file is TagLib.Jpeg.File, "not a Jpeg file");
+        public void CheckTags (File file)
+        {
+            Assert.IsTrue (file is TagLib.Jpeg.File, "not a Jpeg file");
 
-			Assert.AreEqual (contained_types, file.TagTypes);
-			Assert.AreEqual (contained_types, file.TagTypesOnDisk);
-		}
+            Assert.AreEqual (contained_types, file.TagTypes);
+            Assert.AreEqual (contained_types, file.TagTypesOnDisk);
+        }
 
-		public void CheckExif (File file)
-		{
-			var tag = file.GetTag (TagTypes.TiffIFD) as IFDTag;
+        public void CheckExif (File file)
+        {
+            var tag = file.GetTag (TagTypes.TiffIFD) as IFDTag;
 
-			Assert.IsNotNull (tag, "tag");
+            Assert.IsNotNull (tag, "tag");
 
-			var exif_ifd = tag.Structure.GetEntry (0, IFDEntryTag.ExifIFD) as SubIFDEntry;
-			Assert.IsNotNull (exif_ifd, "Exif IFD");
+            var exif_ifd = tag.Structure.GetEntry (0, IFDEntryTag.ExifIFD) as SubIFDEntry;
+            Assert.IsNotNull (exif_ifd, "Exif IFD");
 
-			Assert.AreEqual ("OLYMPUS IMAGING CORP.  ", tag.Make);
-			Assert.AreEqual ("E-410           ", tag.Model);
-			Assert.AreEqual (100, tag.ISOSpeedRatings);
-			Assert.AreEqual (1.0d / 125.0d, tag.ExposureTime);
-			Assert.AreEqual (6.3d, tag.FNumber);
-			Assert.AreEqual (42.0d, tag.FocalLength);
-			Assert.AreEqual (new DateTime (2009, 04, 11, 19, 45, 42), tag.DateTime);
-			Assert.AreEqual (new DateTime (2009, 04, 11, 19, 45, 42), tag.DateTimeDigitized);
-			Assert.AreEqual (new DateTime (2009, 04, 11, 19, 45, 42), tag.DateTimeOriginal);
-		}
+            Assert.AreEqual ("OLYMPUS IMAGING CORP.  ", tag.Make);
+            Assert.AreEqual ("E-410           ", tag.Model);
+            Assert.AreEqual (100, tag.ISOSpeedRatings);
+            Assert.AreEqual (1.0d / 125.0d, tag.ExposureTime);
+            Assert.AreEqual (6.3d, tag.FNumber);
+            Assert.AreEqual (42.0d, tag.FocalLength);
+            Assert.AreEqual (new DateTime (2009, 04, 11, 19, 45, 42), tag.DateTime);
+            Assert.AreEqual (new DateTime (2009, 04, 11, 19, 45, 42), tag.DateTimeDigitized);
+            Assert.AreEqual (new DateTime (2009, 04, 11, 19, 45, 42), tag.DateTimeOriginal);
+        }
 
 
-		public void CheckMakerNote (File file)
-		{
-			var tag = file.GetTag (TagTypes.TiffIFD) as IFDTag;
-			Assert.IsNotNull (tag, "tag");
+        public void CheckMakerNote (File file)
+        {
+            var tag = file.GetTag (TagTypes.TiffIFD) as IFDTag;
+            Assert.IsNotNull (tag, "tag");
 
-			var makernote_ifd =
-				tag.ExifIFD.GetEntry (0, (ushort)ExifEntryTag.MakerNote) as MakernoteIFDEntry;
+            var makernote_ifd =
+                tag.ExifIFD.GetEntry (0, (ushort)ExifEntryTag.MakerNote) as MakernoteIFDEntry;
 
-			Assert.IsNotNull (makernote_ifd, "makernote ifd");
-			Assert.AreEqual (MakernoteType.Olympus2, makernote_ifd.MakernoteType);
+            Assert.IsNotNull (makernote_ifd, "makernote ifd");
+            Assert.AreEqual (MakernoteType.Olympus2, makernote_ifd.MakernoteType);
 
-			var structure = makernote_ifd.Structure;
-			Assert.IsNotNull (structure, "structure");
-			/*{
+            var structure = makernote_ifd.Structure;
+            Assert.IsNotNull (structure, "structure");
+            /*{
 				var entry = structure.GetEntry (0, 0x01) as UndefinedIFDEntry;
 				Assert.IsNotNull (entry);
 				ByteVector read_bytes = entry.Data;
@@ -163,13 +163,13 @@ namespace TaglibSharp.Tests.Images
 				Assert.AreEqual (78.0d/10.0d, (double) values[0]);
 				Assert.AreEqual (78.0d/10.0d, (double) values[1]);
 			}*/
-		}
+        }
 
-		public void CheckProperties (File file)
-		{
-			Assert.AreEqual (3648, file.Properties.PhotoWidth);
-			Assert.AreEqual (2736, file.Properties.PhotoHeight);
-			Assert.AreEqual (96, file.Properties.PhotoQuality);
-		}
-	}
+        public void CheckProperties (File file)
+        {
+            Assert.AreEqual (3648, file.Properties.PhotoWidth);
+            Assert.AreEqual (2736, file.Properties.PhotoHeight);
+            Assert.AreEqual (96, file.Properties.PhotoQuality);
+        }
+    }
 }
